e296ff99c4d83346a05b5d982d9fc0c592d2eae9
Bae gil jun <51225580+bgj0127@users.noreply.github.com>
2020-07-04 18:05:15 +0900

    2016

---
+++ July/4.js
@@ -0,0 +1,10 @@
+//2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
+//입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+//programmers 코딩테스트 연습문제
+
+var a = prompt("월 입력");
+var b = prompt("일 입력");
+
+var day = new Date(2016, a - 1, b); //new Date()에서 month는 0~11까지 있다.(1월이 0, 12월이 11)이걸 몰라서 계속 틀렸다. 잘 알아봐야겠다.
+var date = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+console.log(date[day.getDay()]);
