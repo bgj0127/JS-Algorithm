1c129eb971a8221d55ea09e623c05892283ca5a6
Bae gil jun <51225580+bgj0127@users.noreply.github.com>
2020-07-09 15:42:05 +0900

    Create 9.js

---
+++ July/9.js
@@ -0,0 +1,12 @@
+function solution(strings, n) {
+  strings.sort(function (a, b) {
+    var first = a[n];
+    var second = b[n];
+    if (first === second) {
+      return (a > b) - (a < b);
+    } else {
+      return (first > second) - (first < second);
+    }
+  });
+  return strings;
+}
